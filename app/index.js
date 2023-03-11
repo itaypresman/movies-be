5fdce608106e41fa56df71f7073eeb63da3856ff
Itay Presman <presman.a.g@gmail.com>
2023-03-11 12:54:01 +0200

    CORS fix

--- app/index.js
+++ app/index.js
@@ -2,10 +2,12 @@ const Config = require('@moovies/config');
 const express = require('express');
 const { oimdbApi } = require('./routes/index.js');
 const ErrorMiddleware = require('./middlewares/error.middleware.js');
+const cors = require('cors')
 
 
 const app = express();
 
+app.use(cors());
 app.use('/oimdb', oimdbApi);
 app.use(ErrorMiddleware);
 
